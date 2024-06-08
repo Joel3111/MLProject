64ab6eee9e56968dab111152d2d654c27f455f74
Joel3111 <eglijoe2@students.zhaw.ch>
2024-06-08 16:10:35 +0200

    Update after 2nd Modeltraining

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class_names = ["Normal", "Diabetic Retinopathy", "Cataract", "Glaucoma"]
 
 
 def prepare_image(img_path):
-    img = image.load_img(img_path, target_size=(206, 206))
+    img = image.load_img(img_path, target_size=(256, 256))  # Updated to 256x256
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0
